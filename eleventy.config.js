204aa36a265c1a8a64b01d17a7836b3c8b40be89
Erik Onarheim <erik.onarheim@gmail.com>
2026-09-03 10:02:27 -0500

    add eleventy markdown

---
+++ eleventy.config.js
@@ -0,0 +1,13 @@
+import markdownIt from "markdown-it";
+import footnote_plugin from "markdown-it-footnote";
+
+export default function (eleventyConfig) {
+	let options = {
+		html: true,
+		breaks: true,
+		linkify: true,
+    footnote: true,
+	};
+
+	eleventyConfig.setLibrary("md", markdownIt(options).use(footnote_plugin));
+};
